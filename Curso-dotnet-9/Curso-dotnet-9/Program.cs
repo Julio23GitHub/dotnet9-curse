206e80805d5fbe59d6c8132d3139ed2620910209
Julio23GitHub <Julio23GitHub@users.noreply.github.com>
2025-10-04 15:15:24 -0300

    Configuration for using controllers

--- Curso-dotnet-9/Curso-dotnet-9/Program.cs
+++ Curso-dotnet-9/Curso-dotnet-9/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Inicio del area de servicios 
+
+builder.Services.AddControllers();
+//Fin del area de servicios
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+// Inicio del area de middlewares
+
+app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
+
+// Fin del area de middlewares
 app.Run();
